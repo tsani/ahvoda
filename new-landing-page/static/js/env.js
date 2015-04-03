d9ddcff6dfeb69fe466236c625c3650daebeb725
Jacob Errington <nictytan@gmail.com>
2015-04-03 01:58:10 -0400

    add about section and finalize style

--- new-landing-page/static/js/env.js
+++ new-landing-page/static/js/env.js
@@ -5,3 +5,18 @@ setInterval(function() {
                                .end()
                                .appendTo('.carousel');
 }, 4000);
+
+$(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
