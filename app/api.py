122f54c697be0743ffad08e8abfb191a0a6bbb33
Jacob Errington <nictytan@gmail.com>
2015-04-03 14:30:31 -0400

    add some anti-spam measures in api.py

--- app/api.py
+++ app/api.py
@@ -1,6 +1,6 @@
 from app import app
 
-from flask import jsonify, request
+from flask import jsonify, request, abort
 
 import psycopg2
 
@@ -11,17 +11,31 @@ eprint = lambda *args, **kwargs: print(*args, file=sys.stderr, **kwargs)
 @app.route('/api/addemail', methods=['POST'])
 def add_email():
     req = request.get_json(force=True)
+    if 'email' not in req:
+        response = jsonify({'message': 'no email given'})
+        response.status_code = 400
+        return response
+
     email = req['email']
+    if len(email) > 255:
+        response = jsonify({'message': 'email too long'})
+        resposne.status_code = 400
+        return response
+
     with psycopg2.connect('dbname=%s user=%s password=%s host=%s' %
             tuple(map(lambda s: app.config['DATABASE'][s], ['name', 'user',
                 'password', 'host']))) as conn:
-        print("yoyo")
         cur = conn.cursor()
-        cur.execute('INSERT INTO LandingPageUser ( email ) VALUES ( %s );',
+        cur.execute('SELECT id FROM LandingPageUser WHERE email=%s LIMIT 1;',
                 (email,))
-        #except psycopg2.IntegrityError:
-        #    response = jsonfiy({'message': 'user already exists'})
-        #    response.status_code = 400
-        #    return response
+
+        if cur.rowcount:
+            response = jsonify({'message': 'user already exists'})
+            response.status_code = 400
+            return response
+
+        cur.execute('INSERT INTO LandingPageUser ( email, creator_ip ) '
+                'VALUES ( %s, %s );',
+                (email, request.remote_addr))
         return jsonify({'message': 'ok'})
 
