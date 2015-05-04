b5904965774afd038ca31e13927d33f26fef8598
Jacob Errington <nictytan@gmail.com>
2015-05-04 03:21:51 -0400

    connect create routes for new views

--- app/views.py
+++ app/views.py
@@ -10,3 +10,16 @@ from datetime import datetime
 @app.route('/')
 def index():
     return render_template('landing-page.html')
+
+@app.route('/opportunities')
+def opportunities():
+    return render_template('opportunities.html')
+
+@app.route('/new-listing')
+def new_listing():
+    form = forms.NewListingForm()
+
+    if form.validate_on_submit():
+        return render_template('message.html', message='woot')
+
+    return render_template('new-listing.html', form=form)
