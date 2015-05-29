b1c7163f9b3a23098059ae4249f7b2230568e3db
Jacob Errington <nictytan@gmail.com>
2015-05-29 16:11:15 -0400

    geocoding

--- app/geocoding.py
+++ app/geocoding.py
@@ -1,11 +1,136 @@
 import requests
 import itertools as it
 
+from collections import namedtuple
+from geopy.distance import vincenty
+
 from app import app
 
 _format = 'json'
 _base_url = 'https://api.opencagedata.com/geocode/v1/%s?parameters' % (
         _format,)
+
+GeographicalPosition = namedtuple(
+        'GeographicalPosition', ['latitude', 'longitude'])
+
+class APIRateLimitError(Exception):
+    """ The class of exceptions that arise when an API rate limit is reached.
+    """
+
+    # The specific HTTP error code returned by a service refusing to serve
+    # requests past the limit.
+    ERROR_CODE = 429
+
+class Geocoding:
+    class Result:
+        """ The class of Geocoding results.
+
+            Geocoding is not perfect, and many APIs return multiple results for
+            a given query. In order to tell these results apart, this class
+            presents a number of ways to perform tests on geocoding results. Is
+            this the correct country, state/province, city, etc. ?
+        """
+        def __init__(self, formatted_string, position, components,
+                confidence=0):
+            self.components = components
+            self.formatted_string = formatted_string
+            self.position = position
+            self.confidence = confidence
+
+        def __repr__(self):
+            return 'Result(%s, %s, %s, confidence=%s)' % tuple(
+                    repr(s)
+                    for s in [
+                        self.formatted_string, self.position, self.components,
+                        self.confidence])
+
+    @staticmethod
+    def lookup(query, bounds=None, **kwargs):
+        """ Simply geocode a string.
+
+            Arguments:
+                query (type: string):
+                    The string to geocode. This is a human-readable address
+                    such as '404 rue de l'Introuvable, Montreal, Quebec,
+                    Canada'. It should be a *complete* address, i.e. including
+                    province and country, since it is passed directly to the
+                    geocoding backend.
+
+                bounds (type: tuple of Position objects):
+                    The bounding box of a region to provide a hint to the
+                    OpenCage. This doesn't restrict the region. The format of
+                    the tuple is (southwest corner, northeast corner).
+
+                **kwargs:
+                    Any remaining keyword arguments are simply forwarded to
+                    base_request, which in turn merely forwards them to
+                    OpenCage.
+
+            Exceptions:
+                APIRateLimitError:
+                    If the underlying geocoding API's rate limit is reached,
+                    this error is produced. Specifically, if the HTTP request
+                    to the API returns a 429 error code, then this exception is
+                    raised.
+        """
+
+        l = list(it.chain.from_iterable(
+                    piece()
+                    for t, piece
+                    in [
+                        (True,
+                            lambda: [('q', query)]),
+                        (bounds is not None,
+                            lambda: [(
+                                'bounds',
+                                ','.join(
+                                    ','.join(str(l)
+                                        for l in [p.longitude, p.latitude])
+                                    for p in bounds)
+                                )]
+                            ),
+                        (True, kwargs.items)
+                    ]
+                    if t))
+
+        r = base_request(
+                dict(l))
+
+        return Geocoding.from_opencage(r.json())
+
+    @staticmethod
+    def from_opencage(response):
+        """ Construct a Geocoding instance from a JSON OpenCage HTTP response
+            dictionary.
+
+            Exceptions:
+                APIRateLimitError:
+                    If the underlying geocoding API's rate limit is reached,
+                    this error is produced. Specifically, if the HTTP request
+                    to the API returns a 429 error code, then this exception is
+                    raised.
+        """
+
+        # Sort results by descending order of components. The more components,
+        # the better the match.
+        results = sorted(
+                response['results'],
+                key=lambda r: -len(r['components']))
+
+        results = [
+                Geocoding.Result(
+                    r['formatted'], r['geometry'], r['components'],
+                    len(r['components']))
+                for r in results]
+
+        g = Geocoding(results)
+        return g
+
+    def __init__(self, results):
+        self.results = results
+
+    def __repr__(self):
+        return 'Geocoding(%s)' % ([repr(r) for r in self.results],)
 
 def base_request(params):
     r = requests.get(
@@ -13,5 +138,11 @@ def base_request(params):
             params=dict(it.chain(
                 params.items(),
                 [('key', app.config['OPENCAGE_API_KEY'])])))
+
+    if r.status_code == APIRateLimitError.ERROR_CODE:
+        # TODO change this message to include the time at which more
+        # requests are possible. This is possible with OpenCage. Not sure
+        # for other geocoding APIs.
+        raise APIRateLimitError('no more requests are possible')
 
     return r
