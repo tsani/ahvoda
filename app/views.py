a0bacc5dff86083349147f1a1a5701ba91c512bc
Jacob Errington <nictytan@gmail.com>
2015-06-03 09:07:31 -0400

    remove all routes except for the landing page

--- app/views.py
+++ app/views.py
@@ -10,24 +10,3 @@ from datetime import datetime
 @app.route('/')
 def index():
     return render_template('landing-page.html')
-
-@app.route('/business/create-listing')
-def business_create_listing():
-    return render_template('create-listing.html')
-
-@app.route('/business/login')
-def business_login():
-    return render_template('business-login.html')
-
-@app.route('/opportunities')
-def opportunities():
-    return render_template('opportunities.html')
-
-@app.route('/new-listing')
-def new_listing():
-    form = forms.NewListingForm()
-
-    if form.validate_on_submit():
-        return render_template('message.html', message='woot')
-
-    return render_template('new-listing.html', form=form)
