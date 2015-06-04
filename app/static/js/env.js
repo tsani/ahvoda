f5d0848ac4bba2f8e5f5a07e073c8d7e3eef299c
Jacob Errington <nictytan@gmail.com>
2015-06-04 17:26:15 -0400

    revamp landing page
    * Carousel code completely generalized and reworked.
    * Translucent black filter over landing page carousel.
    * Content of landing page adjusted.

--- app/static/js/env.js
+++ app/static/js/env.js
@@ -1,10 +1,56 @@
-setInterval(function() {
-    $('.carousel :first-child').fadeOut()
-                               .next('img')
-                               .fadeIn()
-                               .end()
-                               .appendTo('.carousel');
-}, 4000);
+$.fn.exists = function () {
+    return this.length !== 0;
+}
+
+var cycleStrategy = {
+    serial: function(container, period) {
+        var e = container.find(':first-child');
+        var next = e.next();
+        e.fadeOut(period/4, function() {
+            next.fadeIn(period/4, function() {
+                e.appendTo(container);
+            });
+        });
+    },
+    parallel: function(container, period) {
+        var e = container.find(':first-child');
+        e.fadeOut(period/4)
+         .next()
+         .fadeIn(period/4, function() {
+             e.appendTo(container);
+         });
+    }
+};
+
+/* Cycles children of a given container by fading them in and out, *serially*.
+ * This is as opposed to fading them in and out in parallel.
+ */
+function cycle(container, strategy) {
+    container = $(container);
+    var speed = container.attr('data-cycle-period');
+    var delay = container.attr('data-cycle-delay');
+
+    if(typeof(speed) === 'undefined') {
+        speed = 2000;
+    }
+
+    if(typeof(speed) === 'undefined') {
+        delay = 0;
+    }
+
+    var update = function() {
+        strategy(container, speed);
+    };
+
+    setTimeout(function() {
+        if(delay > 0)
+            update();
+
+        setInterval(function() {
+            strategy(container, speed)
+        }, speed);
+    }, delay);
+}
 
 $(function() {
     function scrollFadeCheck() {
@@ -36,10 +82,19 @@ $(function() {
             }
         }
     });
+
     /* Every time the window is scrolled ... */
     $(window).scroll(scrollFadeCheck);
 
     scrollFadeCheck();
+
+    $('.cycle-serial').each(function(i, e) {
+        cycle(e, cycleStrategy.serial);
+    });
+
+    $('.cycle-parallel').each(function(i ,e) {
+        cycle(e, cycleStrategy.parallel);
+    });
 });
 
 $(document).ready(function() {
