613db8f6c6712ac68f07fc7b0a5a1f45fa061539
Jacob Errington <nictytan@gmail.com>
2015-06-12 20:25:44 -0400

    configure very basic syslog logging

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,28 @@ from flask.ext.migrate import Migrate
 
 from app.redis_session import RedisSessionInterface
 
+# Initialize application
 app = Flask(__name__)
 app.config.from_object('secret_config')
+
+# Initialize Redis-based server-side sessions
 app.session_interface = RedisSessionInterface()
 
+# Initialize database
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+# Configure logging
+import logging
+from logging.handlers import SysLogHandler
+_syslog_handler = SysLogHandler("/dev/log")
+
+if app.debug:
+    _syslog_handler.setLevel(logging.DEBUG)
+else:
+    _syslog_handler.setLevel(logging.INFO)
+    # TODO log to emails for messages of at least WARNING level
+
+app.logger.addHandler(_syslog_handler)
 
 from app import views, api, forms, models, auth_utils
