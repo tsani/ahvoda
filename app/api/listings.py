a76596bead31c4cdef48bb5e8feae51e32b0ce64
Jacob Errington <nictytan@gmail.com>
2015-06-12 20:45:06 -0400

    add skeleton for /api/listings/create
    Includes a full json schema for the request, and a skeleton schema for
    the response. Uses the decorators for automatic validation and
    authentication verification.

--- app/api/listings.py
+++ app/api/listings.py
@@ -1,9 +1,33 @@
 from app import app, db
 from app import models
+from app import util
+
+import datetime
 
 from flask import request
 
 @app.route('/api/listings/create', methods=['POST'])
-def create():
-    request_data = request.get_json()
+@util.json_validation.validate_response
+@util.auth.requires_auth(failure_handler=util.auth.failure.response_401)
+@util.auth.requires_manager
+@util.json_validation.validate_request
+def create(login):
+    """ API endpoint for creating a new listing -- /api/listings/create
 
+    Request body is JSON of the following form:
+
+        {
+            "name": <position name>,
+            "businessId": <unique identifier for the business>
+            "pay": <amount as a float>,
+            "startTime": <time the shift starts at>
+            "duration":  <employment duration>,
+            "languages": [
+                <language name>,
+                ...
+            ]
+        }
+    """
+    manager = login.get_account()
+
+    return jsonify({"hi": "lol"})
