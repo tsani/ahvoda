d6d824c1fa778a0df06f038aa17b8296c27c540f
Jacob Errington <nictytan@gmail.com>
2015-07-13 17:43:44 -0400

    create utility functions for formatting dates and times

--- app/static/js/src/common/UtilityService.js
+++ app/static/js/src/common/UtilityService.js
@@ -9,4 +9,28 @@ function UtilityService() {
             location.city.state.country.name
         ].join(', ');
     };
+
+    var pad = function(s) {
+        var p = s.toString();
+        var q = p.length === 1 ? '0' + p : p;
+        console.log(q);
+        return q;
+    };
+
+    srv.formatDate = function(dateString) {
+        var d = new Date(dateString);
+        return [
+            d.getFullYear(),
+            pad(d.getMonth()),
+            pad(d.getDay())
+        ].join('-');
+    };
+
+    srv.formatTime = function(dateString) {
+        var d = new Date(dateString);
+        return [
+            pad(d.getHours()),
+            pad(d.getMinutes())
+        ].join(':');
+    };
 }
